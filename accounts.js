774672aa491c98fa08f51571b88426dea13dcc90
Praszad <impraszad@gmail.com>
2023-05-11 23:08:50 +0530

    Update asset transaction

---
+++ accounts.js
@@ -0,0 +1,3 @@
+let addr2Mnemonic =
+  'awesome purse dismiss zoo ridge book practice never jacket angle only tissue envelope ribbon ten enlist final honey yard ridge web cable multiply abandon sorry';
+module.exports = { addr2Mnemonic };
--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const algosdk = require('algosdk');
+const { addr2Mnemonic } = require('./accounts');
+
 const ak = algosdk;
 const MNEMONICS =
   'patrol force swarm purse razor clock next issue erode install wear rack case coast differ silver unknown order film vintage giraffe unhappy top about ahead';
+const account = algosdk.mnemonicToSecretKey(MNEMONICS);
+const algoClient = clientConfig();
 
-async function main() {
-  //   const account = algosdk.generateAccount();
-  //   console.log(account);
-  //   const mnemonics = algosdk.secretKeyToMnemonic(account.sk);
-  //   console.log({ mnemonics });
-  const account = ak.mnemonicToSecretKey(MNEMONICS);
-  //   console.log({ account });
+function createAccount() {
+  const account = algosdk.generateAccount();
+  console.log(account);
+  const mnemonics = algosdk.secretKeyToMnemonic(account.sk);
+  console.log({ mnemonics });
+}
+
+function clientConfig() {
   const algodToken = '';
   const algodServer = 'https://testnet-api.algonode.cloud';
   const algodPort = undefined;
-  const algoClient = new ak.Algodv2(algodToken, algodServer, algodPort);
-  //   console.log(algoClient);
+  const algoC = new ak.Algodv2(algodToken, algodServer, algodPort);
+  console.log(algoC);
+  return algoC;
+}
+async function createAsset() {
+  const account = ak.mnemonicToSecretKey(MNEMONICS);
   const suggestedParams = await algoClient.getTransactionParams().do();
   //   console.log({ suggestedParams });
   const myNewTestAsset = ak.makeAssetCreateTxnWithSuggestedParamsFromObject({
@@ -39,4 +48,27 @@ async function main() {
   console.log(results);
 }
 
-main();
+async function updateAsset() {
+  const clawBackAccount = algosdk.mnemonicToSecretKey(addr2Mnemonic);
+  console.log({ clawBackAccount });
+  let assetIndex = 211214092;
+  let suggestedParams = await algoClient.getTransactionParams().do();
+
+  const updtTxn = ak.makeAssetConfigTxnWithSuggestedParamsFromObject({
+    from: account.addr,
+    clawback: clawBackAccount.addr,
+    reserve: clawBackAccount.addr,
+    assetIndex,
+    suggestedParams,
+    strictEmptyAddressChecking: false
+  });
+  const rawTrxn = updtTxn.signTxn(account.sk);
+  const { txId } = algoClient.sendRawTransaction(rawTrxn).do();
+  const modifiedAsset = await algosdk.waitForConfirmation(algoClient, txId, 8);
+  console.log({ modifiedAsset });
+}
+
+// Function to create asset
+// createAsset();
+
+updateAsset();
